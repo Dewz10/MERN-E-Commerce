8715055945acfb2afaeace155e967b0aefc48c52
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-11 11:52:21 +0700

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send('Hello API Dewdev')
+//@Endpoint http://localhost:3000/api/auth
+//@Method   GET
+//@Access   Publish
+router.get('/auth',(req,res)=>{
+    res.send('Hello API Dewdev GET')
 })
+
+//@Endpoint http://localhost:3000/api/auth
+//@Method   POST
+//@Access   Publish
+router.post('/auth',(req,res)=>{
+    res.send(res.req.body)
+})
+
+//@Endpoint http://localhost:3000/api/auth
+//@Method   PUT
+//@Access   Publish
+router.put('/auth',(req,res)=>{
+    res.send('Hello API Dewdev PUT')
+})
+
+//@Endpoint http://localhost:3000/api/auth
+//@Method   DELETE
+//@Access   Publish
+router.delete('/auth',(req,res)=>{
+    res.send('Hello API Dewdev DELETE')
+})
+
 
 module.exports = router;
