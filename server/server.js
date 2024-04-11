9a02a7a3dd6b11cda37dab9029835b00b4a236b2
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-11 11:31:06 +0700

    SERVER: Router

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const { readdirSync } = require("fs");
 
 const app = express();
 
-const port = '3000';
-app.listen(port,()=>{
-    console.log("Server is running on port " + port);
-})
+// Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "20mb" }));
+app.use(cors());
+
+// Read directory
+readdirSync("./routes").map((r) => app.use('/api', require('./routes/' + r)));
+
+const port = "3000";
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
