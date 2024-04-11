1f6042464336bc50359b94345bc8acca89c5b97b
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-11 17:49:05 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/auth/Login";
 import Register from "./components/pages/auth/Register";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Register/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
