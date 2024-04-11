90418e67f994e909fabaa5e653b24a913ad89b44
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-11 14:35:36 +0700

    SERVER: Login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
   try {
@@ -22,6 +23,39 @@ exports.register = async (req, res) => {
     res.status(500).send("Server Error");
   }
 };
+
+exports.login = async (req,res) => {
+    try{
+        const { username, password } = req.body;
+        var user = await User.findOneAndUpdate({username},{new: true});
+        if (user && user.enabled) {
+            // Check password
+            const isMatch = await bcrypt.compare(password,user.password);
+            
+            if (!isMatch) {
+                return res.status(400).send('Password Invalid!');
+            }
+            // Create payload
+            const payload = {
+                user: {
+                    username: user.username,
+                    role: user.role
+                }
+            }
+            // Gen token
+            jwt.sign(payload,'jwtSecret',{ expiresIn: 3600 },(err,token)=>{
+                if (err) throw err;
+                res.json({token,payload})
+            });
+
+        } else {
+            return res.status(400).send('User not found!');
+        }
+    }catch(err) {
+        console.log(err);
+        res.status(500).send('Server Error!');
+    } 
+}
 
 exports.editUser = async (req, res) => {
   try {
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,27 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Controller
-const {register,listUser,editUser,deleteUser} = require('../controllers/auth');
+const {
+  register,
+  login,
+  listUser,
+  editUser,
+  deleteUser,
+} = require("../controllers/auth");
+
+//@Endpoint http://localhost:3000/api/register
+//@Method   POST
+//@Access   Publish
+router.post("/register", register);
+
+//@Endpoint http://localhost:3000/api/login
+//@Method   POST
+//@Access   Publish
+router.post("/login", login);
 
 //@Endpoint http://localhost:3000/api/auth
 //@Method   GET
 //@Access   Publish
-router.get('/auth',listUser)
-
-//@Endpoint http://localhost:3000/api/auth
-//@Method   POST
-//@Access   Publish
-router.post('/auth',register)
+router.get("/auth", listUser);
 
 //@Endpoint http://localhost:3000/api/auth
 //@Method   PUT
 //@Access   Publish
-router.put('/auth',editUser)
+router.put("/auth", editUser);
 
 //@Endpoint http://localhost:3000/api/auth
 //@Method   DELETE
 //@Access   Publish
-router.delete('/auth',deleteUser)
+router.delete("/auth", deleteUser);
 
 module.exports = router;
