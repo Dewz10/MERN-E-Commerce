4d01ff8ddf1c285f151277ee09a38159bee8b826
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-11 13:15:42 +0700

    SERVER: Create controller

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+// Controller
+const {register,listUser,editUser,deleteUser} = require('../controllers/auth');
+
 //@Endpoint http://localhost:3000/api/auth
 //@Method   GET
 //@Access   Publish
-router.get('/auth',(req,res)=>{
-    res.send('Hello API Dewdev GET')
-})
+router.get('/auth',listUser)
 
 //@Endpoint http://localhost:3000/api/auth
 //@Method   POST
 //@Access   Publish
-router.post('/auth',(req,res)=>{
-    res.send(res.req.body)
-})
+router.post('/auth',register)
 
 //@Endpoint http://localhost:3000/api/auth
 //@Method   PUT
 //@Access   Publish
-router.put('/auth',(req,res)=>{
-    res.send('Hello API Dewdev PUT')
-})
+router.put('/auth',editUser)
 
 //@Endpoint http://localhost:3000/api/auth
 //@Method   DELETE
 //@Access   Publish
-router.delete('/auth',(req,res)=>{
-    res.send('Hello API Dewdev DELETE')
-})
-
+router.delete('/auth',deleteUser)
 
 module.exports = router;
